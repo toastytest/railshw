670c785e028eea3b06f0ccc4b58dae9ca647f58d
Matt Dean <matt.dean@fastmail.com>
2016-03-14 14:02:08 +0000

    Fixed etset

--- helloworld/spec/views/hello/index.html.erb_spec.rb
+++ helloworld/spec/views/hello/index.html.erb_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "hello/index.html.erb", :type => :view do
 #  pending "add some examples to (or delete) #{__FILE__}"
   it "displays hello world" do
     render
-    expect(rendered).to match /Hello World/
+    expect(rendered).to match /Goodbye World/
   end
   it "still displays hello world" do
     render
-    expect(rendered).to match /Hello World/
+    expect(rendered).to match /Goodbye World/
   end
 end
